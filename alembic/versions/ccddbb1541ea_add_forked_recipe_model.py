a9ed3baa0cac5bed9f75cdaf612aa50d3cae838e
Jeet Gaikwad <jeetgaikwad95@gmail.com>
2025-03-05 11:25:57 +0530

    feat: added the Forked Recipes model migration

--- alembic/versions/ccddbb1541ea_add_forked_recipe_model.py
+++ alembic/versions/ccddbb1541ea_add_forked_recipe_model.py
@@ -1,8 +1,8 @@
-"""Add Recipe model
+"""Add Forked Recipe model
 
-Revision ID: cccf9b0ed470
-Revises: 69f4f3bed347
-Create Date: 2025-03-05 11:02:51.051515
+Revision ID: ccddbb1541ea
+Revises: 19eb4b4c5d1b
+Create Date: 2025-03-05 11:17:18.759511
 
 """
 from typing import Sequence, Union
@@ -12,33 +12,31 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'cccf9b0ed470'
-down_revision: Union[str, None] = '69f4f3bed347'
+revision: str = 'ccddbb1541ea'
+down_revision: Union[str, None] = '19eb4b4c5d1b'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.create_table('recipes',
+    op.create_table('forked_recipes',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('userId', sa.Integer(), nullable=False),
+    sa.Column('recipeId', sa.Integer(), nullable=False),
     sa.Column('recipeName', sa.String(length=255), nullable=False),
     sa.Column('description', sa.String(length=500), nullable=True),
     sa.Column('recipeType', sa.Enum('veg', 'nonveg', name='tag'), nullable=False),
     sa.Column('peopleCount', sa.Integer(), nullable=True, server_default=sa.text('1')),
-    sa.Column('likesCount', sa.Integer(), nullable=True, server_default=sa.text('0')),
-    sa.Column('forkedCount', sa.Integer(), nullable=True, server_default=sa.text('0')),
-    sa.Column('isDeleted', sa.Boolean(), nullable=True, server_default=sa.text('0')),
-    sa.Column('isHide', sa.Boolean(), nullable=True, server_default=sa.text('0')),
     sa.Column('createdAt', sa.DateTime(), nullable=False, server_default=sa.func.now()),
     sa.Column('updatedAt', sa.DateTime(), nullable=True, server_default=sa.func.now(), onupdate=sa.func.now()),
-    sa.Column('deletedAt', sa.DateTime(), nullable=True),
+    sa.ForeignKeyConstraint(['recipeId'], ['recipes.id'], ),
     sa.ForeignKeyConstraint(['userId'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
-    op.create_index(op.f('ix_recipes_id'), 'recipes', ['id'], unique=False)
+    op.create_index(op.f('ix_forked_recipes_id'), 'forked_recipes', ['id'], unique=False)
 
 
 def downgrade() -> None:
-    op.drop_index(op.f('ix_recipes_id'), table_name='recipes')
-    op.drop_table('recipes')
+    op.drop_index(op.f('ix_forked_recipes_id'), table_name='forked_recipes')
+    op.drop_table('forked_recipes')
+
