b2e3fe145465da7ac9a3a9ef62de7d0c9f1e0a56
Jeet Gaikwad <jeetgaikwad95@gmail.com>
2025-03-05 11:38:43 +0530

    feat: added the Recipe Likes model migration

---
+++ alembic/versions/7366fafd3e06_add_recipe_likes_model.py
@@ -0,0 +1,34 @@
+"""Add Recipe Likes model
+
+Revision ID: 7366fafd3e06
+Revises: 2dc5017ad955
+Create Date: 2025-03-05 11:36:41.937652
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '7366fafd3e06'
+down_revision: Union[str, None] = '2dc5017ad955'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table('recipe_likes',
+    sa.Column('userId', sa.Integer(), nullable=False),
+    sa.Column('recipeId', sa.Integer(), nullable=False),
+    sa.Column('createdAt', sa.DateTime(), nullable=False, server_default=sa.func.now()),
+    sa.ForeignKeyConstraint(['recipeId'], ['recipes.id'], ),
+    sa.ForeignKeyConstraint(['userId'], ['users.id'], ),
+    sa.PrimaryKeyConstraint('userId', 'recipeId')
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('recipe_likes')
+
